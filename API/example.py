751ccaa3315a2ce9bd55ff7c7a2f8a3806ac1196
DucNguyen <ducnguyenpy1221@gmail.com>
2021-08-14 00:30:27 +0700

    [DONE]-[DONE] remove trash class

--- API/example.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import requests
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=700000,VN&appid=10531eec7d08826697eaa0211d932abb')
-json_object = r.text
-print(json_object)
-
